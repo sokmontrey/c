33f4b52b44ae7a6637185187519b2a62c4aff0b2
Sokmontrey <sythatsokmontrey@gmail.com>
2024-02-10 16:58:39 -0500

    new hasmap code test

--- hashmap.c
+++ hashmap.c
@@ -1,101 +1,146 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define SIZE 1000
-
-unsigned long hash(const char *str) {
-  unsigned long i = 0;
-  for (int j = 0; str[j]; j++)
-    i += str[j];
-  return i % SIZE;
+size_t createHash(const char *key, size_t size) {
+  size_t hash = 0;
+  int c;
+  while ((c = *key++))
+    hash = c + (hash << 6) + (hash << 16) - hash;
+  return hash % size;
 }
 
 typedef struct Item {
   char *key;
-  int value;
+  double value;
+  struct Item *next;
 } Item;
 
-typedef struct HashTable {
-  Item **items;
-  int size;
-  int count;
-} HashTable;
-
-Item *createItem(const char *key, int value) {
+Item *createItem(const char *key, double value) {
   Item *item = (Item *)malloc(sizeof(Item));
-
   item->key = (char *)malloc(strlen(key) + 1);
   strcpy(item->key, key);
+  item->value = value;
+  item->next = NULL;
+  return item;
+}
 
+typedef struct {
+  size_t size;
+  size_t count;
+  Item **items;
+} HashMap;
+
+HashMap *createHashMap(size_t size) {
+  HashMap *hashmap = (HashMap *)malloc(sizeof(HashMap));
+  hashmap->size = size;
+  hashmap->items = (Item **)malloc(sizeof(Item *) * size);
+
+  for (int i = 0; i < size; i++)
+    hashmap->items[i] = NULL;
+
+  return hashmap;
+}
+
+Item *getHead(HashMap *hashmap, const char *key) {
+  size_t index = createHash(key, hashmap->size);
+  return hashmap->items[index];
+}
+
+double get(HashMap *hashmap, const char *key) {
+  Item *head = getHead(hashmap, key);
+  while (head != NULL) {
+    if (strcmp(head->key, key) == 0)
+      return head->value;
+    head = head->next;
+  }
+  fprintf(stderr, "HashMap: get: Key not found: %s\n", key);
+  exit(-1);
+}
+
+Item *set(Item *item, double value) {
   item->value = value;
   return item;
 }
 
-HashTable *createHashTable(int size) {
-  HashTable *table = (HashTable *)malloc(sizeof(HashTable));
-  table->size = size;
-  table->count = 0;
-  table->items = (Item **)calloc(size, sizeof(Item *));
+Item *put(HashMap *hashmap, const char *key, double value) {
+  size_t index = createHash(key, hashmap->size);
+  Item *head = hashmap->items[index];
 
-  for (int i = 0; i < table->size; i++)
-    table->items[i] = NULL;
+  if (head == NULL) {
+    hashmap->count++;
+    return hashmap->items[index] = createItem(key, value);
+  }
 
-  return table;
+  for (;;) {
+    if (head == NULL)
+      break;
+    if (strcmp(head->key, key) == 0)
+      return set(head, value);
+    if (head->next == NULL)
+      break;
+  }
+
+  hashmap->count++;
+  return head->next = createItem(key, value);
 }
 
-void printHashTable(HashTable *table) {
-  for (int i = 0; i < table->size; i++) {
-    Item *item = table->items[i];
-    if (item != NULL)
-      printf("Index = %d, Key = %s, Value = %d\n", i, item->key, item->value);
+void print(HashMap *hashmap) {
+  for (int i = 0; i < hashmap->size; i++) {
+    Item *head = hashmap->items[i];
+    if (head == NULL)
+      continue;
+    printf("%d: ", i);
+    while (head != NULL) {
+      printf("%s: %f, ", head->key, head->value);
+      head = head->next;
+    }
+    printf("\n");
   }
+}
+
+Item **getItems(HashMap *hashmap) {
+  Item **items = (Item **)malloc(sizeof(Item *) * hashmap->count);
+
+  int j = 0;
+  for (int i = 0; i < hashmap->size; i++) {
+    Item *head = hashmap->items[i];
+    while (head != NULL) {
+      items[j++] = head;
+      head = head->next;
+    }
+  }
+
+  return items;
 }
 
 void freeItem(Item *item) {
+  if (item == NULL)
+    return;
   free(item->key);
+  freeItem(item->next);
   free(item);
 }
 
-void freeHashTable(HashTable *table) {
-  for (int i = 0; i < table->size; i++) {
-    Item *item = table->items[i];
-    if (item != NULL)
-      freeItem(item);
-  }
-
-  free(table->items);
-  free(table);
+void freeHashMap(HashMap *hashmap) {
+  for (int i = 0; i < hashmap->size; i++)
+    freeItem(hashmap->items[i]);
+  free(hashmap->items);
+  free(hashmap);
 }
 
-void insert(HashTable *table, const char *key, int value) {
-  int index = hash(key);
-  Item* new_item = createItem(key, value);
-  Item* item = table->items[index];
+int main() {
+  HashMap *hashmap = createHashMap(20);
 
-  if (item == NULL) {
-    table->items[index] = new_item;
-    table->count++;
-    return;
-  }
-}
+  put(hashmap, "a", 10.0);
+  put(hashmap, "b", 20.0);
+  put(hashmap, "bcd", 30.0);
+  put(hashmap, "dcb", 40.0);
 
-int main() { 
+  print(hashmap);
 
-  // Create a new hash table
-  // Insert some values
-  // Print the hash table
-  // Free the hash table
-  
-  HashTable *table = createHashTable(SIZE);
+  freeHashMap(hashmap);
 
-  insert(table, "a", 1);
-  insert(table, "b", 2);
-  insert(table, "c", 3);
-
-  printHashTable(table);
-
-  freeHashTable(table);
-
-  return 0; 
+  return 0;
 }
